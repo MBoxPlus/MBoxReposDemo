741687466708294b48651a4974c841d213926859
杨彬 <yangbin.jacky@bytedance.com>
2021-08-27 14:15:23 +0800

    init

---
+++ Example/Pods/Target Support Files/MBoxReposDemo/MBoxReposDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MBoxReposDemo : NSObject
+@end
+@implementation PodsDummy_MBoxReposDemo
+@end
---
+++ Example/Pods/Target Support Files/Pods-MBoxReposDemo_Example/Pods-MBoxReposDemo_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MBoxReposDemo_Example : NSObject
+@end
+@implementation PodsDummy_Pods_MBoxReposDemo_Example
+@end
